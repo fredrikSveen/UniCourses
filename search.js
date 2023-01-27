816535d8447eab3f0f47cbd3b612048c9467d4c6
salwaZahi <44439874+salwaZahi@users.noreply.github.com>
2023-01-27 13:12:08 +0100

    new updates on search html css js and icons

--- search.js
+++ search.js
@@ -1,29 +1,67 @@
-function myFunction() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("courses");
-    tr = table.getElementsByTagName("tr");
+function searchFunction() {
+	// Declare variables
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("searchInput");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("courses");
+	tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-        } else {
-            tr[i].style.display = "none";
-        }
-        }
-    }
+	  // Loop through all table rows, and hide those who don't match the search query
+	  for (i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+		  txtValue = td.textContent || td.innerText;
+		  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			tr[i].style.display = "";
+		  } else {
+			tr[i].style.display = "none";
+		  }
+		}
+	  }
 }
 
-function myFunction1() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function searchECTSF() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchECTS");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("courses");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+	td = tr[i].getElementsByTagName("td")[2];
+	if (td) {
+	  txtValue = td.textContent || td.innerText;
+	  if (txtValue == filter || filter == '') {
+		tr[i].style.display = "";
+	  } else {
+		tr[i].style.display = "none";
+	  }
+	}
+  }
 }
 
-for (var i = 0, l = courses.length; i < l; i++) {
-    courses[i].showCourse();
-}
+function searchScoreF() {
+  console.log('hi');
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchScore");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("courses");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+	td = tr[i].getElementsByTagName("td")[3];
+	if (td) {
+	  txtValue = td.textContent || td.innerText;
+	  console.log(txtValue);
+	if (txtValue >= filter){
+		tr[i].style.display = "";
+	  } else {
+		tr[i].style.display = "none";
+	  }
+	}
+  }
+}	
